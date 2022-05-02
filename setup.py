ba5ae08f8716f2b249a4bc41bf3e8a89997c1863
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2022-05-02 05:30:53 +0530

    Bump versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 _requires = [
     'setuptools-scm',
-    'ebs-linuxnode-core',
+    'ebs-linuxnode-core>=2.4',
     'raspi_system>=0.1.2',  # Only used on RPi
     'ifcfg',
     'tendril-utils-core',
